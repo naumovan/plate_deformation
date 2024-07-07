206380a3f9282f84c4879744b1031c1a9aeddd0a
Oladiy <bd3396121@gmail.com>
2024-07-07 03:50:18 +0200

    Fix test

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -7,8 +7,10 @@ import PIL.Image
 
 def test_flow_single_request():
     image_path = "fixtures/0b90c1c74d46abf7.jpg"
-    image = cv2.imread(image_path)
-    image_bytes = pickle.dumps(image)
+    image = PIL.Image.open(image_path)
+    with io.BytesIO() as byte_stream:
+        image.save(byte_stream, format='JPEG')
+        image_bytes = byte_stream.getvalue()
     files = {'file': ('filename', image_bytes)}
     r = requests.post('http://127.0.0.1:8000/', files=files, timeout=20)
     print("Plate = ", r.headers["X-Text"])
